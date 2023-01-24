2fb115176f94482f52bb2a46e6a6594c91af7285
StamGio <stamatisgio@icloud.com>
2023-01-24 15:30:17 +0200

    Comments for The Utils and functions

--- client/src/Portofoliocontainer/AboutMe/AboutMe.js
+++ client/src/Portofoliocontainer/AboutMe/AboutMe.js
@@ -3,6 +3,7 @@ import ScreenHeading from "../../Utilities/ScreenHeading/ScreenHeading";
 import ScrollHeading from "../../Utilities/ScrollService";
 import Animations from "../../Utilities/Animations";
 import ScrollService from "../../Utilities/ScrollService";
+import "./AboutMe.css";
 
 export default function AboutMe(props) {
   let fadeInScreenHandler = (screen) => {
@@ -13,10 +14,59 @@ export default function AboutMe(props) {
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
+  const SCREEN_CONSTANTS = {
+    description:
+      "Full stack web developer with background knowledge of MERN stack,Strong professional with willing to become a high performing member of a team and asset for an organization",
+    highlights: {
+      bullets: [
+        "Full Stack Development",
+        "Manage and create Databases",
+        "Building REST API",
+        "MERN stack applications",
+        "Passion for learning",
+      ],
+      heading: "Here are a Few Highlights",
+    },
+  };
+
+  const renderHighlight = () => {
+    return SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
+      <div className="highlight" key={i}>
+        <div className="highlight-blob"></div>
+        <span>{value}</span>
+      </div>
+    ));
+  };
+
   return (
     <div className="about-me-container screen-container" id={props.id || ""}>
       <div className="about-me-parent">
         <ScreenHeading title={"About me"} subHeading={"Why Choose Me?"} />
+        <div className="about-me-card">
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <span className="about-me-description">
+              {SCREEN_CONSTANTS.description}
+            </span>
+            <div className="about-me-highlights">
+              <div className="highlight-heading">
+                <span>{SCREEN_CONSTANTS.highlights.heading}</span>
+              </div>
+              {renderHighlight()}
+            </div>
+            <div className="about-me-options">
+              <button
+                className="btn primary-btn"
+                onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+              >
+                {""} Hire Me{" "}
+              </button>
+              <a href="stamatis giovanis.pdf" download="stamatis giovanis.pdf">
+                <button className="btn highlighted-btn">Get Resume</button>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
