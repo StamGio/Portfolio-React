092cd5f13263495dfd1e3741d7480fe57e15dcc9
StamGio <stamatisgio@icloud.com>
2023-02-13 20:04:53 +0200

    reset

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-const path = require("path");
+// const path = require("path");
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-app.listen(port, (err) => {
-  if (err) return console.log(err);
-  console.log("Server running on port: ", port);
-});
+// app.listen(port, (err) => {
+//   if (err) return console.log(err);
+//   console.log("Server running on port: ", port);
+// });
